c82ccde3dede62f959eee27b973627a5572e4143
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2018-07-03 00:00:21 +0200

    Preview page added

--- library/src/javascripts/app.js
+++ library/src/javascripts/app.js
@@ -5,89 +5,101 @@ const Granim = require('granim');
 const WebMidi = require('webmidi');
 const screenfull = require('screenfull');
 
-$(document).ready(() => {
-	if ($('.preview').length) {
-		WebMidi.enable(function(err) {
-			let input;
-			for (let i = 0; i < WebMidi.inputs.length; i++) {
-				console.log(WebMidi.inputs[i].name);
-				input = '<div class="input">';
-				input += WebMidi.inputs[i].name;
-				input += '</div>';
-				$('.midi').append(input);
-			}
+// preview and test
+function preview() {
+	WebMidi.enable(function(err) {
+		let inputList;
+		for (let i = 0; i < WebMidi.inputs.length; i++) {
+			inputList = '<div class="input">';
+			inputList += WebMidi.inputs[i].name;
+			inputList += '</div>';
+			$('.midi').append(inputList);
+		}
+		let input = WebMidi.getInputByName($('.data').data('input'));
+		input.addListener('noteon', 'all', function(e) {
+			let note = e.note.name + e.note.octave;
+			window.alert(note);
 		});
-		$.getJSON($('.data').data('url'), function(data) {
-			let track;
-			for (let i = 0; i < Object.keys(data).length; i++) {
-				track = '<div class="track">';
-				track +=
-					'<h3 class="track__title">' +
-					Object.keys(data)[i] +
-					'</h3>';
-				track +=
-					'<div class="track__gradient" style="background: -webkit-linear-gradient(-45deg, ' +
-					Object.values(data)[i].gradients[0][0] +
-					' 0%,' +
-					Object.values(data)[i].gradients[0][1] +
-					' 100%);"></div>';
-				track +=
-					'<div class="track__gradient" style="background: -webkit-linear-gradient(-45deg, ' +
-					Object.values(data)[i].gradients[2][0] +
-					' 0%,' +
-					Object.values(data)[i].gradients[2][1] +
-					' 100%);"></div>';
-				track += '</div>';
-				$('.preview').append(track);
-			}
-		});
-	} else {
-		$.getJSON($('.data').data('url'), function(data) {
-			// init Granim
-			const granimInstance = new Granim({
-				element: '#canvas-interactive',
-				name: 'interactive-gradient',
-				elToSetClassOn: '.canvas-interactive-wrapper',
-				direction: 'diagonal',
+	});
+	$.getJSON($('.data').data('url'), function(data) {
+		let track;
+		for (let i = 0; i < Object.keys(data).length; i++) {
+			track = '<div class="track">';
+			track +=
+				'<h3 class="track__title">' + Object.keys(data)[i] + '</h3>';
+			track +=
+				'<canvas id="track__gradient--' +
+				i +
+				'" class="track__gradient"></canvas>';
+			track += '</div>';
+			$('.preview').append(track);
+			new Granim({
+				element: '#track__gradient--' + i,
+				name: 'granim',
 				opacity: [1, 1],
-				isPausedWhenNotInView: true,
-				stateTransitionSpeed: 5000,
-				states: data
-			});
-
-			// init WebMidi
-			WebMidi.enable(function(err) {
-				if (err) {
-					console.log('WebMidi could not be enabled.', err);
-				} else {
-					let input = WebMidi.getInputByName(
-						$('.data').data('input')
-					);
-					console.log(input);
-					// receive midi
-					input.addListener('noteon', 'all', function(e) {
-						let note = e.note.name + e.note.octave;
-
-						// change gradient
-						$.getJSON($('.data').data('midi'), function(data) {
-							let obj = data.filter(function(midi) {
-								return midi.midi == note;
-							});
-							if (obj.length !== 0) {
-								let name = Object.values(obj[0])[0];
-								granimInstance.changeState(name);
-							}
-						});
-					});
+				states: {
+					'default-state': {
+						gradients: Object.values(data)[i].gradients,
+						transitionSpeed: 7000
+					}
 				}
 			});
+		}
+	});
+}
+
+// live
+function live() {
+	$.getJSON($('.data').data('url'), function(data) {
+		// init Granim
+		const granimInstance = new Granim({
+			element: '#canvas-interactive',
+			name: 'interactive-gradient',
+			elToSetClassOn: '.canvas-interactive-wrapper',
+			direction: 'diagonal',
+			opacity: [1, 1],
+			isPausedWhenNotInView: true,
+			stateTransitionSpeed: 5000,
+			states: data
 		});
 
-		// fullscreen
-		$('.live__zone').click(function() {
-			if (screenfull.enabled) {
-				screenfull.request();
+		// init WebMidi
+		WebMidi.enable(function(err) {
+			if (err) {
+				console.log('WebMidi could not be enabled.', err);
+			} else {
+				let input = WebMidi.getInputByName($('.data').data('input'));
+				// receive midi
+				input.addListener('noteon', 'all', function(e) {
+					let note = e.note.name + e.note.octave;
+
+					// change gradient
+					$.getJSON($('.data').data('midi'), function(data) {
+						let obj = data.filter(function(midi) {
+							return midi.midi == note;
+						});
+						if (obj.length !== 0) {
+							let name = Object.values(obj[0])[0];
+							granimInstance.changeState(name);
+						}
+					});
+				});
 			}
 		});
+	});
+
+	// fullscreen
+	$('.live__zone').click(function() {
+		if (screenfull.enabled) {
+			screenfull.request();
+		}
+	});
+}
+
+$(document).ready(() => {
+	if ($('.preview').length) {
+		preview();
+	} else {
+		live();
 	}
 });
